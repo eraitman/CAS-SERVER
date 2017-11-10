c1762e01bf6a7d47c80e8c61c5e9af7f2cd7b6d1
eraitman <eraitman@naver.com>
2017-11-10 18:42:44 +0900

    config수정

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ module.exports =
     sessionSecret : 'skfkakfTkalebdrnrdpEkfdk@!#',
     db : {
         host:'localhost',
-        port:3306,
         user:'cliffDev',
-        password:'skfk@akfTkal'
+        password:'skfk@akfTkal',
+        database : 'CASDB'
     }
 }
